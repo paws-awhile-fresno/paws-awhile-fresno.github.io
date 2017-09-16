390f25587bd23e0ea281c3b3fac07054ff5363ee
Ryan Lee <ryantlee9@gmail.com>
2017-09-15 19:07:16 -0700

    Use pricing form to fill message placeholder
    TODO: copy placeholder to message on click

--- js/priceCalculator.js
+++ js/priceCalculator.js
@@ -1,9 +1,10 @@
 var pricingForm = document.forms["pricing"];
+var type;
 
 function getTypePricing() {
   // Get relevant pricing form for pet type
-  var type = pricingForm.elements["type"];
-  return pricingDict[type.value];
+  type = pricingForm.elements["type"].value;
+  return pricingDict[type];
 }
 
 function calculateTotal()
@@ -13,13 +14,50 @@ function calculateTotal()
 
   var price = 0;
   var checkable = ['bath', 'shave', 'nails', 'sentry'];
+  var services = [];
   for (var i = 0; i < checkable.length; i++) {
     boxName = checkable[i];
     if (pricingForm.elements[boxName].checked) {
       price += prices[boxName] * 1;
+      placeholderText += 'a ' + boxName + ', '
+      services.push(boxName)
     }
   }
-
+  
+  // update message placeholder
+  var petTypeDescriptions = {
+      "d0": "dog (< 25 lbs)",
+      "d25": "dog (25-50 lbs)",
+      "d50": "dog (50-75 lbs)",
+      "d75": "dog (> 75 lbs)",
+      "cs": "shorthair cat",
+      "cl": "longhair cat"
+  };
+  var serviceDescriptions = {
+      "shave": "shave",
+      "nails": "nail trim",
+      "sentry": "sentry application",
+      "bath": "bath (with brush and blowdry)"
+  };
+  var placeholderText = "Hi Kelly, I would like ";
+  var nServices = services.length;
+  placeholderText += "a " + serviceDescriptions[services[0]];
+  for (var i = 1; i < nServices - 1; i++) {
+      if (i > 0 && nServices > 2) {
+          placeholderText += ', ';
+      }
+      placeholderText += 'a ' + serviceDescriptions[services[i]];
+  }
+  if (nServices > 1) {
+      if (nServices > 2) {
+          placeholderText += ",";
+      }
+      placeholderText += " and a " + serviceDescriptions[services[nServices - 1]];
+  }
+  placeholderText += " for my " + petTypeDescriptions[type] + ".";
+  document.getElementById('message-box').value = placeholderText;
+  
   //display the result
   document.getElementById('totalPrice').innerHTML = "Your initial price estimate is $" + Math.max(50, price) + ". Kindly note that this estimate is not final. Please call (559) 978-5052 for a final quote -- every pet is unique, and actual pricing may vary.";
 }
+
